ed6b3a86670e51422ea14d1bc7e4a6a8ad8c38b1
ofdove <SennKou528@gmail.com>
2026-01-12 21:17:20 +0900

    Change default theme to everforest.

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -7,11 +7,25 @@ return {
       styles = "moon",
     },
   },
+  {
+    "neanias/everforest-nvim",
+    lazy = true,
+    config = function()
+      require("everforest").setup({
+        -- your options here
+        background = "hard",
+        transparent_background_level = 0.5,
+      })
+
+      -- Set the colorscheme
+      vim.cmd("colorscheme everforest")
+    end,
+  },
 
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "tokyonight",
+      colorscheme = "everforest",
     },
   },
 }
